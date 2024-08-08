d3ec226b02268d46a8fe5a016d89e30b97154c95
Yrving <yrving.molina@mo2o.com>
2024-08-08 13:28:48 -0500

    Initial Commit

---
+++ ViewHomeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViewHomeApp
+//
+//  Created by Yrving Heysen Molina Yriarte on 8/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewHomeApp/ViewHomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewHomeAppApp.swift
+//  ViewHomeApp
+//
+//  Created by Yrving Heysen Molina Yriarte on 8/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ViewHomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
