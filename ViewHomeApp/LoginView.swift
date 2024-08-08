940f3a7e73d4282f7b36a98bd1813fb4d87c0a6f
Yrving <yrving.molina@mo2o.com>
2024-08-08 14:18:51 -0500

    init app

---
+++ ViewHomeApp/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  ViewHomeApp
+//
+//  Created by Yrving Heysen Molina Yriarte on 8/08/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
